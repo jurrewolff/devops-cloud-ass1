2d891525fed27bc4f7af0dd735b87a567e2ff54e
Jurre Wolff <dev@jurrewolff.nl>
2023-02-08 19:02:48 +0100

    Replace tinydb with mongodb.

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,13 @@
-FROM python:3.6-alpine
+FROM python:3.8-alpine
 
-RUN mkdir -p /usr/src/app
-WORKDIR /usr/src/app
+WORKDIR /app
 
-COPY requirements.txt /usr/src/app/
-
+COPY requirements.txt ./
 RUN pip3 install --no-cache-dir -r requirements.txt
 
-COPY . /usr/src/app
+COPY ./ ./
 
 EXPOSE 8080
-
 ENTRYPOINT ["python3"]
-
 CMD ["-m", "swagger_server"]
+
--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -1,41 +1,38 @@
+import json
 import os
 import tempfile
 from functools import reduce
 
 from tinydb import TinyDB, Query
+from pymongo import MongoClient
 
 db_dir_path = tempfile.gettempdir()
 db_file_path = os.path.join(db_dir_path, "students.json")
 student_db = TinyDB(db_file_path)
 
+# DB config and init
+connectionString = "mongodb://root:verysecure@127.0.0.1:27017/devopscloud?authSource=admin"
+dbClient = MongoClient(connectionString)
+db = dbClient["student"]
+collection = db["info"]
+
 
 def add(student=None):
-    queries = []
-    query = Query()
-    queries.append(query.first_name == student.first_name)
-    queries.append(query.last_name == student.last_name)
-    query = reduce(lambda a, b: a & b, queries)
-    res = student_db.search(query)
-    if res:
-        return 'already exists', 409
-
-    doc_id = student_db.insert(student.to_dict())
-    student.student_id = doc_id
+    collection.insert_one(student.to_dict())
     return student.student_id
 
 
 def get_by_id(student_id=None, subject=None):
-    student = student_db.get(doc_id=int(student_id))
+    student = collection.find_one({'student_id': student_id})
     if not student:
         return 'not found', 404
-    student['student_id'] = student_id
-    print(student)
+
+    student.pop("_id")
     return student
 
 
 def delete(student_id=None):
-    student = student_db.get(doc_id=int(student_id))
+    student = collection.find_one_and_delete({'student_id': student_id})
     if not student:
         return 'not found', 404
-    student_db.remove(doc_ids=[int(student_id)])
     return student_id
